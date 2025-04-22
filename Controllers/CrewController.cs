94308764d24e15d757e4059ca1d9e01a6fbf7ddb
Mardoqueo <jeffreymardoqueo260@gmail.com>
2025-04-22 09:18:53 -0600

    Creacion de Crew

---
+++ Controllers/CrewController.cs
@@ -0,0 +1,31 @@
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OnePieceWorld.service;
+
+namespace OnePieceWorld.Controllers
+{
+    [Route("[controller]")]
+    public class CrewController : Controller
+    {
+        private readonly ICrewService _crewService;
+
+        public CrewController (ICrewService crewService){
+            _crewService = crewService;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var crews = await _crewService.GetCrewsAsync();
+            return View(crews);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
